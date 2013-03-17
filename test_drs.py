406024c327434fd02b3dfa3a03b4da8635a5d0be
Friedrich Weber <fred@reichbier.de>
2013-03-17 13:27:39 +0100

    Removed some obsolete test scripts.

--- test_drs.py
+++
@@ -1,7 +0,0 @@
-from genie import drs, slp
-
-with open('aoe2/graphics.drs', 'r') as f:
-    for resource_type, resource_id, data in drs.get_all_files(f):
-        filename = '%d.%s' % (resource_id, drs.get_file_extension(resource_type))
-        with open(filename, 'w') as g:
-            g.write(data)
