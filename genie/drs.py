b11cdea3682a9443a21538568bc5950310797e04
Taryn Hill <Phrohdoh@gmail.com>
2015-09-20 21:06:25 -0500

    Parse SWGB DRS archives

--- genie/drs.py
+++ genie/drs.py
@@ -42,8 +42,17 @@ TABLE = cons.Struct('tables',
     )
 )
 
-HEADER = cons.Struct('header',
+AOE_HEADER = cons.Struct('header',
     cons.String('copyright', 40, padchar='\0'),
+    cons.String('version', 4),
+    cons.String('file_type', 12, padchar='\0'),
+    cons.ULInt32('number_of_tables'),
+    cons.ULInt32('offset'),
+    cons.Array(lambda ctx: ctx['number_of_tables'], TableAdapter(TABLE)),
+)
+
+SWGB_HEADER = cons.Struct(
+    cons.String('copyright', 60, padchar='\0'),
     cons.String('version', 4),
     cons.String('file_type', 12, padchar='\0'),
     cons.ULInt32('number_of_tables'),
@@ -106,7 +115,15 @@ class DRSFile(object):
     """
     def __init__(self, stream):
         self.stream = stream
-        self.header = HEADER._parse(stream, cons.Container(drs_file=self))
+        pos = stream.tell()
+        stream.seek(64)
+        maybe_swgb_header = stream.read(4)
+        stream.seek(pos)
+
+        if maybe_swgb_header == "swbg":
+            self.header = SWGB_HEADER._parse(stream, cons.Container(drs_file=self))
+        else:
+            self.header = AOE_HEADER._parse(stream, cons.Container(drs_file=self))
 
     @property
     def tables(self):
